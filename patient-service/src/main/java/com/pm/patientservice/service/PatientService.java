56bd166eaf8fe17a1e62a5694a83b2d01f1e8b48
Vijayasandeep <vijayasandeepyellamilli@gmail.com>
2026-01-31 12:55:14 +0530

    first commit

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,10 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+}
